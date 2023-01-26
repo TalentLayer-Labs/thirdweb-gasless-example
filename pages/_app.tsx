3477f7cc13b65e529112ade9cd6a856a63d66773
mattiapomelli <mattiapomelli@gmail.com>
2023-01-26 17:34:47 +0000

    Comment open zeppelin config

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -13,13 +13,13 @@ if (!process.env.NEXT_PUBLIC_BICONOMY_API_ID) {
   throw new Error("NEXT_PUBLIC_BICONOMY_API_ID is not defined");
 }
 
-if (!process.env.NEXT_PUBLIC_OZ_RELAYER_URL) {
-  throw new Error("NEXT_PUBLIC_OZ_RELAYER_URL is not defined");
-}
+// if (!process.env.NEXT_PUBLIC_OZ_RELAYER_URL) {
+//   throw new Error("NEXT_PUBLIC_OZ_RELAYER_URL is not defined");
+// }
 
 const NEXT_PUBLIC_BICONOMY_API_KEY = process.env.NEXT_PUBLIC_BICONOMY_API_KEY;
 const NEXT_PUBLIC_BICONOMY_API_ID = process.env.NEXT_PUBLIC_BICONOMY_API_ID;
-const NEXT_PUBLIC_OZ_RELAYER_URL = process.env.NEXT_PUBLIC_OZ_RELAYER_URL;
+// const NEXT_PUBLIC_OZ_RELAYER_URL = process.env.NEXT_PUBLIC_OZ_RELAYER_URL;
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
@@ -31,9 +31,9 @@ const App = ({ Component, pageProps }: AppProps) => {
             apiKey: NEXT_PUBLIC_BICONOMY_API_KEY,
             apiId: NEXT_PUBLIC_BICONOMY_API_ID,
           },
-          openzeppelin: {
-            relayerUrl: NEXT_PUBLIC_OZ_RELAYER_URL,
-          },
+          // openzeppelin: {
+          //   relayerUrl: NEXT_PUBLIC_OZ_RELAYER_URL,
+          // },
         },
       }}
     >
